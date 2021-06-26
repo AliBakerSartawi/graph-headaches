9d9d550655d571adb7f16b049a977fa33cbe69ca
AliBakerSartawi <alisartawi@live.com>
2021-06-26 15:12:04 +0300

    airports example

---
+++ airports-bfs-dfs.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// Vertices
+const airports = 'PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM'.split(' ');
+
+// Edges
+const routes = [
+  ['PHX', 'LAX'],
+  ['PHX', 'JFK'],
+  ['JFK', 'OKC'],
+  ['JFK', 'HEL'],
+  ['JFK', 'LOS'],
+  ['MEX', 'LAX'],
+  ['MEX', 'BKK'],
+  ['MEX', 'LIM'],
+  ['MEX', 'EZE'],
+  ['LIM', 'BKK'],
+];
+
+// The graph
+const adjacencyList = new Map();
+
+// Add node
+function addNode(airport) {
+  adjacencyList.set(airport, [])
+}
+
+// Add edge, undirected, weightless
+function addEdge(origin, destination) {
+  adjacencyList.get(origin).push(destination)
+  adjacencyList.get(destination).push(origin)
+}
